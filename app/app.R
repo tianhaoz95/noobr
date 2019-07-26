44b4102663f60e5fb777c63241f96423aee8d237
Tianhao Zhou <tianhaoz@umich.edu>
2019-07-26 11:10:20 -0700

    Update app.R

--- app/app.R
+++ app/app.R
@@ -3,14 +3,21 @@ library(shiny)
 ui <- fluidPage(
     titlePanel("My App"),
     mainPanel(
-        textOutput(outputId = "test")
+        textOutput(outputId = "test"),
+        fileInput("img_file", "Choose Image File",
+        accept = c(
+          "image/png, image/jpg",
+          "imgae/png, image/jpg",
+          ".png, .jpg")
+        )
     )
 )
 
 server <- function(input, output) {
     output$test <- renderText({
         "test this"
-    })
+    }),
+    img_file <- input$img_file
 }
 
 shinyApp(ui = ui, server = server)
